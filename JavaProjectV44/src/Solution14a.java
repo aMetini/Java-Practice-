2e7faf2ccbb7f874e96377b8ade87ec777e30217
aMetini <amberella1981@yahoo.com>
2022-11-03 08:59:24 +0100

    updated code to fix some bugs in solution 14a and 18.

--- JavaProjectV44/src/Solution14a.java
+++ JavaProjectV44/src/Solution14a.java
@@ -1,22 +1,20 @@
 public class Solution14a {
     public static void main(String[] args) throws Exception {
         int i = 0;
-        //int oddSquare = 0;
+        int oddSquare = 0;
         int sumOddSquared = 0;
 
         do{
             if (i % 2 != 0) {
-                //oddSquare = (int) Math.pow(i, 2);
-                //sumOddSquared += oddSquare;
-                sumOddSquared += (i * i);
-                //System.out.println(sumOddSquared);
-                //System.out.println(oddSquare + " ");
-                System.out.println("The sum of all odd numbers squared, between 0 - 10 is: " + sumOddSquared);
+                oddSquare = (int) Math.pow(i, 2);
+                sumOddSquared += oddSquare;
+                //sumOddSquared += (i * i);
             }
                 i++;
                 
                 
         }while (i <= 10);
+        System.out.println("The sum of all odd numbers squared, between 0 - 10 is: " + sumOddSquared);
     }
     
 }
